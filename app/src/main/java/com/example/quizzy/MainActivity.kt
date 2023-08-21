30969e77b5648e87b09fcbe6332374b7b6767f04
Summer <samar.khaleed.309@Gmail.com>
2023-08-21 19:31:02 +0300

    logic done

--- app/src/main/java/com/example/quizzy/MainActivity.kt
+++ app/src/main/java/com/example/quizzy/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.quizzy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnStart : Button = findViewById(R.id.btn_start)
+        val etName : EditText = findViewById(R.id.et_name)
+
+        btnStart.setOnClickListener{
+            if(etName.text.isEmpty()){
+                Snackbar.make(it, "Please enter your name.", Snackbar.LENGTH_LONG).show()
+                Log.d(String(),"button clicked")
+            } else{
+                val intent = Intent(this,QuizQuestions::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/quizzy/QuizQuestions.kt
@@ -0,0 +1,195 @@
+package com.example.quizzy
+import android.graphics.Color
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.example.quizzy.R.layout.activity_quiz_questions
+import com.google.android.material.snackbar.Snackbar
+
+
+class QuizQuestions : AppCompatActivity() , View.OnClickListener{
+    //Its better to use view Binding
+    private var progressBar : ProgressBar? = null
+    private var tvProgress : TextView? = null
+    private var tvQuestion : TextView? = null
+    private var imgQuestion : ImageView? = null
+
+    private var optionOne : TextView? = null
+    private var optionTwo : TextView? = null
+    private var optionThree : TextView? = null
+    private var optionFour : TextView? = null
+    private var nextBtn : Button? = null
+
+    private var currentPosition = 1
+    private var selectedOption: Int = 0
+    private var questionList : ArrayList<Question>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_quiz_questions)
+
+        progressBar =findViewById(R.id.progress_bar)
+        tvProgress =findViewById(R.id.tv_progress)
+        tvQuestion =findViewById(R.id.tv_question)
+        imgQuestion =findViewById(R.id.img_question)
+        optionOne =findViewById(R.id.tv_option_one)
+        optionTwo =findViewById(R.id.tv_option_two)
+        optionThree =findViewById(R.id.tv_option_three)
+        optionFour =findViewById(R.id.tv_option_four)
+        nextBtn  = findViewById(R.id.btn_next)
+
+        questionList = Constants.getQuestions()
+        setQuestion()
+
+        optionOne?.setOnClickListener(this)
+        optionTwo?.setOnClickListener(this)
+        optionThree?.setOnClickListener(this)
+        optionFour?.setOnClickListener(this)
+        nextBtn?.setOnClickListener(this)
+    }
+
+    private fun setQuestion() {
+        defaultOptionView()
+        val currentQuestion: Question = questionList!![currentPosition - 1]
+
+        progressBar?.progress = currentPosition
+        tvProgress?.text = "$currentPosition / ${progressBar?.max}"
+        tvQuestion?.text = currentQuestion.question
+        imgQuestion?.setImageResource(currentQuestion.img)
+        optionOne?.text = currentQuestion.optionOne
+        optionTwo?.text = currentQuestion.optionTwo
+        optionThree?.text = currentQuestion.optionThree
+        optionThree?.text = currentQuestion.optionThree
+        optionFour?.text = currentQuestion.optionFour
+
+        if(currentPosition == questionList!!.size){
+            nextBtn?.text = "Submit"
+        }else{
+            nextBtn?.text =" Next"
+        }
+    }
+
+    private fun defaultOptionView(){
+        val options = ArrayList<TextView>()
+
+        optionOne?.let {
+            options.add(0 , it)
+        }
+        optionTwo?.let {
+            options.add(1 , it)
+        }
+        optionThree?.let {
+            options.add(2 , it)
+        }
+        optionFour?.let {
+            options.add(3 , it)
+        }
+
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this,R.drawable.default_option_border_bg)
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView , selectedOptionNum : Int){
+        defaultOptionView()
+
+        selectedOption = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#610DB5"))
+        tv.setTypeface(tv.typeface , Typeface.BOLD )
+        tv.background = ContextCompat.getDrawable(this,R.drawable.selected_option_border_bg)
+    }
+
+    private fun answerView(answer : Int , drawableView: Int){
+        when(answer){
+            1 ->  optionOne?.background = ContextCompat.getDrawable(this,drawableView)
+            2 ->  optionTwo?.background = ContextCompat.getDrawable(this,drawableView)
+            3 ->  optionThree?.background = ContextCompat.getDrawable(this,drawableView)
+            4 ->  optionFour?.background = ContextCompat.getDrawable(this,drawableView)
+        }
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.tv_option_one -> {
+                optionOne?.let {
+                    selectedOptionView(it,1)
+                }
+            }
+            R.id.tv_option_two -> {
+                optionTwo?.let {
+                    selectedOptionView(it,2)
+                }
+            }
+            R.id.tv_option_three -> {
+                optionThree?.let {
+                    selectedOptionView(it,3)
+                }
+            }
+            R.id.tv_option_four -> {
+                optionFour?.let {
+                    selectedOptionView(it,4)
+                }
+            }
+
+            R.id.btn_next -> {
+                if (selectedOption == 0) {
+                    currentPosition++
+
+                    when {
+                        currentPosition <= questionList!!.size -> {
+                            setQuestion()
+                        }else ->{
+                        Snackbar.make(view, "Questions done", Snackbar.LENGTH_LONG).show()
+                    }
+                    }
+                } else {
+                    val question = questionList?.get(currentPosition - 1)
+                    if (question!!.correctAnswer != selectedOption) {
+                        answerView(selectedOption, R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (currentPosition == questionList!!.size) {
+                        nextBtn?.text = "Finish"
+                    } else {
+                        nextBtn?.text = "Go to next question"
+                    }
+                    selectedOption = 0;
+                }
+            }
+
+        }
+    }
+
+    /*private fun nextOnClick(){
+        val nextBtn : Button = findViewById(R.id.btn_next)
+        nextBtn.setOnClickListener{
+            if(currentPosition < questionList.size){
+            currentPosition++
+            val currentQuestion : Question = questionList[currentPosition]
+                progressBar?.progress = currentPosition
+                tvProgress?.text = "$currentPosition/${progressBar?.max}"
+                tvQuestion?.text = currentQuestion.question
+                imgQuestion?.setImageResource(currentQuestion.img)
+                optionOne?.text = currentQuestion.optionOne
+                optionTwo?.text = currentQuestion.optionTwo
+                optionThree?.text = currentQuestion.optionThree
+                optionThree?.text = currentQuestion.optionThree
+                optionFour?.text = currentQuestion.optionFour
+                }
+            else {
+                Snackbar.make(it, "Questions done", Snackbar.LENGTH_LONG).show()
+
+            }
+        }*/
+
+    }
